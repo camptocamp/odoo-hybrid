5321a60d1e9bb6f8954524be4bf77c8c9bf38bb1
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2026-09-23 00:01:55 +0200

    fix: licence and copyright

--- src/odoo_hybrid/server.py
+++ src/odoo_hybrid/server.py
@@ -1,3 +1,6 @@
+# Copyright 2026 Camptocamp SA
+# Copyright 2015 Odoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 from __future__ import annotations
 
 import argparse
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+# Copyright 2026 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)import argparse
 import sys
 from unittest.mock import patch
 
